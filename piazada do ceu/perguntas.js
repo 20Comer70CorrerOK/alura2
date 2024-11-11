865a112d7698fe55f59230af05e72e245aa794aa
20Comer70CorrerOK <148350329+20Comer70CorrerOK@users.noreply.github.com>
2024-11-11 10:50:18 -0300

    Add files via upload

---
+++ piazada do ceu/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Enxadrista',
+    'Quem e o rei da velha guarda do xadrez',
+    'Bobby Fischer'
+)
+
+criaCartao(
+    'Disputas',
+    'Qual a nação que tem mais feitos no xadrez',
+    'A Russia por conta da União sovietica na epoca da Guerra Fria'
+)
+
+criaCartao(
+    'Enxadrista',
+    'Quem era para suceder Fischer mas desanimou',
+    'Magnus Carlsen (Foi jogar para o gremio)'
+)
+
+criaCartao(
+    'Abertura',
+    'Caracterizase por D4, seguido de C4, se recusado CC3',
+    'Gambito da Dama, com uma % avasaladora dessa variante ser jogada contra a India do Rei'
+)
